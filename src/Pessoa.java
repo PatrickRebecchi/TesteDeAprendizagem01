cba0edcf99903cc4c182055d42b7417635bf5b22
Patrick <patrickrebecchi@hotmail.com>
2023-07-17 14:32:49 -0300

    classe pessoa finalizada

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,29 @@
 public class Pessoa{
-    
+     String nome;
+     String documento;
+     int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
